ccf695d380b92cc4b95520891ec4bf6f7f2e8c68
MFarhanBaloch <muhammadfarhanbaloch3809632@gmail.com>
2025-02-25 17:26:00 +0500

    Add files via upload

---
+++ Week3/userInfo.java
@@ -0,0 +1,17 @@
+public class userInfo{
+
+private static String userName="abc";
+private static String passWord="123";
+
+public String getUsername(){
+return userName;
+
+}
+
+public String getPassword(){
+return passWord;
+
+}
+
+
+} 
